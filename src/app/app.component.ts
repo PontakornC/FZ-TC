58fa06664f8c97d4f1f94e037017d72a0e44a51d
PontakornC <pontakorn.chokmanusorn@gmail.com>
2025-08-24 15:33:13 +0700

    update icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { Position, positions } from './positions.model';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, RouterOutlet, CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
